050341c5c9bc35c7b3c45307a0d748afd2f53c0e
Ridhi-1234 <98380576+Ridhi-1234@users.noreply.github.com>
2022-01-25 15:43:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 18;
+        int b = 6;
+        Pro_2.branching(a, b);
+        Pro_2.compare();
+        Pro_2.compare2();
+        Pro_2.ifElse();
+        Pro_2.ifElse2();
+        Pro_2.whileLoop();
+        Pro_2.doWhileLoop();
+        Pro_2.forLoop();
+        Pro_2.nestedLoop();
+        Pro_2.isDivsibleBy3();
+
+    }
+}
