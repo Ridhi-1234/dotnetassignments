f8d206dce231651d20f5f69a669da026097fe17d
Ridhi-1234 <98380576+Ridhi-1234@users.noreply.github.com>
2022-01-25 15:49:24 +0530

    Add files via upload

---
+++ Programmain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 18;
+        int b = 6;
+        int c = 2;
+        int d = 3;
+        double x = 5;
+        double y = 4;
+        double z = 2;
+        Pro_1.add(a, b);
+        Pro_1.sub(a, b);
+        Pro_1.mul(a, b);
+        Pro_1.div(a, b);
+        Pro_1.complex(a, b, c);
+        Pro_1.complex2(a, b, c, d);
+        Pro_1.findMinMax();
+        Pro_1.doubleArithametics(x, y, z);
+        Pro_1.roundingError();
+        Pro_1.decimalMinMax();
+        Pro_1.decimalVsDouble();
+        Pro_1.areaOfCircle();
+
+    }
+}
